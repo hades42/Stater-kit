fdd714541dd41796ae641d44ef727ab281f8442a
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2020-04-28 13:06:40 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import { run } from "./app/app";
+import "./scss/main.scss";
+// import { AlertService } from "./app/alert.service";
+// import { ComponentService } from "./app/component.service";
+
+// const alertService = new AlertService();
+// const componentService = new ComponentService();
+// console.log("HI HI");
+// run(alertService, componentService);
